a142b162ca3d567f78e915724963a950b80922f3
ssaitala.dev <ssaitala.dev@gmail.com>
2015-01-31 04:45:18 +0530

    Basic Hibernate operation

---
+++ HelloWorld/src/main/java/net/viralpatel/hibernate/Main.java
@@ -0,0 +1,48 @@
+package net.viralpatel.hibernate;
+
+import java.sql.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Write
+		System.out.println("******* WRITE *******");
+		Employee empl = new Employee("Jack", "Bauer", new Date(
+				System.currentTimeMillis()), "911");
+		empl.setId(20L);
+		empl = save(empl);
+
+	}
+
+	private static Employee save(Employee employee) {
+		SessionFactory sf = buildSessionFactory();
+		Session session = sf.openSession();
+
+		session.beginTransaction();
+
+		Long id = (Long) session.save(employee);
+
+		session.getTransaction().commit();
+
+		session.close();
+
+		return employee;
+	}
+	
+	private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new AnnotationConfiguration()
+            		.configure()
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+}
